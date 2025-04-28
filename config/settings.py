d14f74b8552e00147a2333d3b37c1561d6315ef6
csmatthew <christopher.s.matthew@gmail.com>
2025-04-28 23:02:59 +0100

    Fix duplicate WhiteNoise middleware

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
