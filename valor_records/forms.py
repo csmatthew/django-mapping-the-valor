4f34e7381ef0fb755e39420e0096ccad07319790
csmatthew <christopher.s.matthew@gmail.com>
2025-05-05 03:32:36 +0100

    feat: modal allows view and edit

--- valor_records/forms.py
+++ valor_records/forms.py
@@ -7,20 +7,6 @@ class ValorRecordForm(forms.ModelForm):
         model = ValorRecord
         fields = [
             "name",
-            "record_type",
-            "deanery",
             "dedication",
-            "house_type",
-            "religious_order",
-            "latitude",
-            "longitude",
+
         ]
-        widgets = {
-            "name": forms.TextInput(attrs={"class": "form-control"}),
-            "record_type": forms.Select(attrs={"class": "form-select"}),
-            "deanery": forms.Select(attrs={"class": "form-select"}),
-            "house_type": forms.Select(attrs={"class": "form-select"}),
-            "religious_order": forms.Select(attrs={"class": "form-select"}),
-            "latitude": forms.NumberInput(attrs={"class": "form-control"}),
-            "longitude": forms.NumberInput(attrs={"class": "form-control"}),
-        }
